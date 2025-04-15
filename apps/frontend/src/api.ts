01b0302f9f90b4d2fed1d9625d6e0b5980ff3df0
otetoteto <oteto2oteto@gmail.com>
2025-04-16 00:13:40 +0900

    hono on tanstack/start

--- apps/frontend/src/api.ts
+++ apps/frontend/src/api.ts
@@ -1,6 +1,21 @@
 import {
-  createStartAPIHandler,
-  defaultAPIFileRouteHandler,
+  createStartAPIHandler
 } from '@tanstack/react-start/api'
+import { Hono } from 'hono'
+import { hc } from 'hono/client'
 
-export default createStartAPIHandler(defaultAPIFileRouteHandler)
+const app = new Hono()
+const app1 = app.get("/api/hoge/foo", (c) => {
+  return c.text("hoge foo")
+})
+const app2 = app1.post("/api/piyo", (c) => {
+  return c.json({message: "piyo"})
+})
+
+type App = typeof app2
+
+export const client = hc<App>("http://localhost:3000")
+
+export default createStartAPIHandler(async ({request}) => {
+  return await app.fetch(request)
+})
